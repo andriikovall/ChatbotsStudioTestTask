6517175023799c295a812dd0bfc065c8ced8b931
ziovio <anderkoval3@gmail.com>
2020-01-27 13:42:57 +0200

    added lint, lint-fix, start and dev scripts

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@ const express = require('express');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get('*', (req, res) => {
   res.send('Hello world');
 });
 
-app.listen(3000, () => console.log('app listening on port 3000'));
+app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
