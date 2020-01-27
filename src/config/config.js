2e446774df265f120e02e4e6d53410d8b0a410c6
ziovio <anderkoval3@gmail.com>
2020-01-27 19:51:04 +0200

    added salt and jwt_secret info config

--- src/config/config.js
+++ src/config/config.js
@@ -4,4 +4,6 @@ module.exports = {
   DB_URL_TEMPLATE: process.env.DB_URL_TEMPLATE,
   DB_USERNAME: process.env.DB_USERNAME,
   DB_PASSWORD: process.env.DB_PASSWORD,
+  SALT: process.env.SALT || 'supersecret,rlv,d;lvdf,',
+  JWT_SECRET: process.env.JWT_SECRET || 'super-secret.ksklslmfskmlvfmkl',
 };
