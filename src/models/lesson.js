15dd77026e3cbd0983f2f361775955bdd501face
ziovio <anderkoval3@gmail.com>
2020-01-27 15:34:35 +0200

    added ref pror to schema

--- src/models/lesson.js
+++ src/models/lesson.js
@@ -4,12 +4,12 @@ const shortid = require('shortid');
 const lessonSchema = new mongoose.Schema({
   _id: { type: String, default: shortid.generate },
   name: { type: String, required: true },
-  teacher: { type: String, required: true },
-  group: { type: String, required: true },
+  teacher: { type: String, required: true, ref: 'Teacher' },
+  group: { type: String, required: true, ref: 'Group' },
   place: { type: String, required: true },
-  indexNumber: { type: String, required: true }
+  indexNumber: { type: String, required: true },
 });
 
-const lessonModel = new mongoose.model('cart', lessonSchema);
+const lessonModel = new mongoose.model('lesson', lessonSchema);
 
 module.exports = lessonModel;
