5e27768e7a9f96ef12a7c55dac4e8b787b38f953
ziovio <anderkoval3@gmail.com>
2020-01-27 18:16:38 +0200

    added urlencoding to avoid possible issues

--- src/db/index.js
+++ src/db/index.js
@@ -3,8 +3,8 @@ const { DB_URL_TEMPLATE, DB_USERNAME, DB_PASSWORD } = require('../config/config'
 
 function connectToDB(cb) {
   const dbUrl = DB_URL_TEMPLATE
-    .replace('<username>', DB_USERNAME)
-    .replace('<password>', DB_PASSWORD);
+    .replace('<username>', encodeURIComponent(DB_USERNAME))
+    .replace('<password>', encodeURIComponent(DB_PASSWORD));
 
   mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true }, cb);
 }
