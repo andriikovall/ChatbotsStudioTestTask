8b1c5b9b243b0c5f6cd3c3ba2f908358a5a762b2
ziovio <anderkoval3@gmail.com>
2020-01-28 13:07:28 +0200

    added student model

---
+++ src/models/student.js
@@ -0,0 +1,13 @@
+const shortid = require('shortid');
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema({
+  _id: { type: String, default: shortid.generate },
+  name: { type: String, required: true },
+  group: { type: String, default: null, ref: 'Group' },
+}, { versionKey: false });
+
+
+const studentModel = new mongoose.model('lesson', studentSchema);
+
+module.exports = studentModel;
