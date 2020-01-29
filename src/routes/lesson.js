5d1831a097980f3f18bd3e74fa43913363f89064
ziovio <anderkoval3@gmail.com>
2020-01-29 18:18:05 +0200

    changed routes proccesing sequence

--- src/routes/lesson.js
+++ src/routes/lesson.js
@@ -3,8 +3,8 @@ const controllers = require('../controllers/lesson');
 
 const router = express.Router();
 
-router.get('/:id', controllers.getById);
 router.get('/search', controllers.get);
+router.get('/:id', controllers.getById);
 router.delete('/:id', controllers.delete);
 router.put('/:id', controllers.update);
 router.post('/new', controllers.insert);
--- src/routes/teacher.js
+++ src/routes/teacher.js
@@ -3,8 +3,8 @@ const controllers = require('../controllers/teacher');
 
 const router = express.Router();
 
-router.get('/:id', controllers.getById);
 router.get('/search', controllers.get);
+router.get('/:id', controllers.getById);
 router.delete('/:id', controllers.delete);
 router.put('/:id', controllers.update);
 router.post('/new', controllers.insert);
