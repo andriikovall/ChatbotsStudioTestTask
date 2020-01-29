c6b62ad13fe9a764e5f281d6641b3c2b4847e2b2
ziovio <anderkoval3@gmail.com>
2020-01-29 15:19:23 +0200

    rm required tag from spec code

--- src/models/group.js
+++ src/models/group.js
@@ -5,7 +5,7 @@ const groupSchema = new mongoose.Schema({
   _id: { type: String, default: shortid.generate },
   name: { type: String, required: true, unique: true },
   students: [{ type: String, required: true, ref: 'Student' }],
-  specialisationCode: { type: Number, required: true, unique: true },
+  specialisationCode: { type: Number, required: true },
 }, { versionKey: false });
 
 const groupModel = new mongoose.model('group', groupSchema);
